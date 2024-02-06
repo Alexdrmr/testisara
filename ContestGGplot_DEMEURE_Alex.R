f3021495df911e3d4e3efc48ec82979f6178618d
alexdrmr <ademeure@etu.isara.fr>
2024-02-06 17:35:14 +0100

    voici le graphique le moche

---
+++ ContestGGplot_DEMEURE_Alex.R
@@ -0,0 +1,40 @@
+#DEMEURE Alex
+#GGplot 2
+
+library(ggplot2)
+# Création d'un jeu de données fictif
+df <- data.frame( category = rep(c("A", "B", "C"), each = 100),
+  x = rnorm(300),
+  y = rnorm(300))
+
+# Création d'un graphique avec plusieurs couches
+ggplot(df, mapping = aes(x = x, y = y, color = category)) +
+  geom_point(size = 30) +
+  geom_smooth(method = "lm", linetype = "dashed", color = "black") +
+  facet_wrap(~category) +
+  labs(title = "Graphique avec plusieurs couches",
+       x = "Variable X",
+       y = "Variable Y") +
+  geom_density_2d_filled()+
+  geom_polygon()+
+  
+
+  ggplot(df, mapping = aes(x = x, y = y, color = category)) +
+  geom_point(size = 1) + geom_smooth(method = "lm", linetype = "dashed",
+                                     color = "black") + facet_wrap(~category) + labs(title = "Graphique avec
+plusieurs couches", x = "Variable X", y = "Variable Y")
+  
+#geom_point(): Ajoute un nuage de points coloré par catégorie.
+#geom_smooth(): Ajoute une ligne de régression linéaire (droite de tendance) en pointillés.
+#facet_wrap(): Divise le graphique en sous-graphiques par catégorie.
+#labs(): Ajoute des étiquettes au graphique.
+#geom_density_2d_filled() : Ajoute des contours remplis basés sur la densité 2D des données
+#geom_polygon() : Ajoute des polygones (vous devez fournir des données supplémentaires pour définir les polygones)
+
+
+
+
+
+
+
+
